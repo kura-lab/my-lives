e8de9c95e0076373f9ba39411c0bdd82c2946f0b
kura <mkurahay@gmail.com>
2017-09-23 17:49:56 +0900

    added artists and songs count.

--- list-generator.php
+++ list-generator.php
@@ -12,9 +12,28 @@ $list = scandir($dir);
 $filename = 'list.json';
 
 $files = [];
+$artists = [];
+$songs = [];
 foreach ($list as $file) {
   if (preg_match('/.*\.json\z/', $file) && $file !== $filename) {
     $files[] = $file;
+    $row = file_get_contents($dir . '/' . $file);
+    $json = json_decode($row, true);
+    if (!empty($json['setlist'])) {
+      foreach ($json['setlist'] as $sets) {
+        foreach ($sets as $set) {
+          if (!empty($set[0])) {
+            $splited = explode(',', $set[0]);
+            foreach ($splited as $artist) {
+              $artists[] = trim($artist);
+            }
+          }
+          if (!empty($set[1])) {
+            $songs = array_merge($songs, $set[1]);
+          }
+        }
+      }
+    }
   }
 }
 
@@ -29,4 +48,13 @@ echo $visualized;
 
 file_put_contents($dir . '/' . $filename, $visualized);
 
-echo "\n\nwrote event list to $dir/$filename\n";
+echo "\n\nwrote event list to $dir/$filename\n\n";
+
+$countArtists = count($artists);
+$countSongs = count($songs);
+sort($artists);
+sort($songs);
+$uniqueArtists = count(array_unique($artists));
+$uniqueSongs = count(array_unique($songs));
+echo "artists: $countArtists (unique $uniqueArtists)\n";
+echo "songs:   $countSongs (unique $uniqueSongs)\n";
